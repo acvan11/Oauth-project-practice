aa031c6254f3f41f3d9fff75a4e514ef1374baa0
acvan11 <anvanc37@gmail.com>
2019-05-31 22:49:05 -0700

    initial setup to deploy to heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.use('/', (req, res) => {
+	res.send('Our First Express program');
+})
+
+app.listen(port);
