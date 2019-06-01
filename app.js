2c4248dffe6d10b37068f1fa5d0cb3fff1d910d5
acvan11 <anvanc37@gmail.com>
2019-06-01 11:20:50 -0700

    update with auth route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const authRoutes = require('./routes/auth-routes.js');
 const app = express();
 const port = process.env.PORT || 3000
 
 // setup view engine
 app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth',authRoutes);
 
 // create home route
 app.get('/', (req, res) => {
